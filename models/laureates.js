706e8b64de8fef6792558dcfac1a0fee34b69e76
sit79 <stefan.tolksdorf@gmail.com>
2019-05-23 14:44:51 +0200

    initial commit

---
+++ models/laureates.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const LaureateSchema = new Schema({
+    born: String,
+    bornCity: String,
+    bornCountry: String,
+    bornCountrycode: String,
+    died: String,
+    diedCity: String,
+    diedCountry: String,
+    diedCountrycode: String,
+    firstname: String,
+    gender: String,
+    userID: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    prizes: [
+        {
+            affiliations: [
+                {
+                    city: String,
+                    country: String,
+                    name: String
+                }
+            ],
+            category: String,
+            movitvation: String,
+            share: String,
+            year: String
+        }
+    ],
+    surname: String
+})
+
+const Laureate = mongoose.model("Laureate", LaureateSchema)
