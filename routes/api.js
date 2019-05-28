be7c7c7753781e905cdef154f3aab528d45f35b2
sit79 <stefan.tolksdorf@gmail.com>
2019-05-28 14:18:14 +0200

    post route added

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const router = express.Router()
-const chalk = require("chalk")
 const Laureate = require("../models/laureates")
 
 /* GET home page. */
@@ -8,14 +7,28 @@ router.get("/", (req, res) => {
     res.json({ response: "Welcome to this API" })
 })
 
-router.get("/api/laureates", async (req, res) => {
-    console.log(chalk.blue("request is being processed"))
-    const allLaureates = await Laureate.find({
-        bornCity: "Berlin",
-        diedCity: "Berlin"
-    })
-    console.log(chalk.red(allLaureates.length))
-    res.json(allLaureates)
+router.get("/api/laureates/:bornCity", async (req, res) => {
+    const searchTerm = req.params.bornCity
+
+    try {
+        const allLaureates = await Laureate.find({
+            bornCity: searchTerm
+        })
+        res.status(200).send(allLaureates)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+router.post("/api/laureates/", async (req, res) => {
+    const newLaureate = new Laureate(req.body)
+
+    try {
+        await newLaureate.save()
+        res.send(newLaureate)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 module.exports = router
